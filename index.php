eb814b644a9a0da8a51adfde7f582c4c119014aa
mynameismako <43156617+mynameismako@users.noreply.github.com>
2019-01-13 14:35:13 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+	<div class="bg">
+	<div class="navigation">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="index.php"><img src="lib/logo.png" style="width: 200px; height: 50px;"><img></a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="nav justify-content-right">
+      <li class="nav-item active">
+        <a class="nav-link" href="#!">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#!">Portfolio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#!">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#!">Press</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#!">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
+</div>
+<div class="container">
+<div class="space"></div>
+<div class="line"> </div>
+<div class="space"> 
+<h4 style="text-align: center;">DEDICATED TO CREATING CURATED, MEMORABLE WEDDINGS AND EVENTS</h4>
+</div>
+<div class="line"> </div>
+<div class="space"></div>
+
+<p style="text-align: center; color: grey; font-style: italic;">brides magazine</p>
+<h5 style="text-align: center; font-style: italic;">NAMED BEST WEDDING PLANNER IN THE U.S. </h5>
+<div class="space"></div>
+	<img style="height: 160px; width: 170px;" src="lib/planning logo.jpg" class="center">
+	<div class="space"></div>
+<div class="line"></div>
+<div class="space"></div>
+<h3 style="text-align: center; color: #67949d"> OUR WORK</h3>
+</div>
+<div class="container">
+	<img src="/images/pathToYourImage.png" class="img-fluid" alt="Responsive image">
+	<img src="/images/pathToYourImage.png" class="img-fluid" alt="Responsive image">
+	<img src="/images/pathToYourImage.png" class="img-fluid" alt="Responsive image">
+</div>
+</body>
+</html>
